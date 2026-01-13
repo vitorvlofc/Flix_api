5beeda371a1e356ae87aaddc4f14db6d009506d4
vitor <vitorvlofc@users.noreply.github.com>
2026-01-13 09:43:12 -0300

    143 - ModelSerializer e Validações

--- movies/serializers.py
+++ movies/serializers.py
@@ -4,23 +4,18 @@ from genres.models import Genre
 from actors.models import Actor
 
 
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField()
-
-    genre = serializers.PrimaryKeyRelatedField(
-        queryset=Genre.objects.all()
-    )
-    release_date = serializers.DateField()
-    actors = serializers.PrimaryKeyRelatedField(
-        queryset=Actor.objects.all(),
-        many=True
-    )
-
-    resume = serializers.CharField()
-
-
-class MovieModelSerializer(serializers.ModelSerializer):
+class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError("A data de lançamento não pode ser anterior a 1990.")
+        return value
+    
+
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError("O resumo não deve ser maior que 200 caracteres.")
+        return value
