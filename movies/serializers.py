ab19041ff0fe818b056d40e38f6f6edeb12d91c7
vitor <vitorvlofc@users.noreply.github.com>
2026-01-13 09:12:05 -0300

    142 - Escrevendo um serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,8 +1,26 @@
 from rest_framework import serializers
-from movies.models import Movie 
+from movies.models import Movie
+from genres.models import Genre
+from actors.models import Actor
 
 
-class MovieSerializer(serializers.ModelSerializer):
+class MovieSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+
+    genre = serializers.PrimaryKeyRelatedField(
+        queryset=Genre.objects.all()
+    )
+    release_date = serializers.DateField()
+    actors = serializers.PrimaryKeyRelatedField(
+        queryset=Actor.objects.all(),
+        many=True
+    )
+
+    resume = serializers.CharField()
+
+
+class MovieModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
