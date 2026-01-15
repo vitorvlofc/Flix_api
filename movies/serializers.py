b385040e85cc1bab5e0583a3d94054ea9c1bc426
vitor <vitorvlofc@users.noreply.github.com>
2026-01-14 21:26:45 -0300

    145 - Calculando rate (parte 1)

--- movies/serializers.py
+++ movies/serializers.py
@@ -9,15 +9,16 @@ class MovieModelSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_rate(self, obj):
+        reviews = obj.review.all()
         return 5
 
     def validate_release_date(self, value):
-        if value.year < 1990:
-            raise serializers.ValidationError("A data de lançamento não pode ser anterior a 1990.")
+        if value.year < 1900:
+            raise serializers.ValidationError("A data de lançamento não pode ser anterior a 1900.")
         return value
     
 
     def validate_resume(self, value):
-        if len(value) > 200:
-            raise serializers.ValidationError("O resumo não deve ser maior que 200 caracteres.")
+        if len(value) > 500:
+            raise serializers.ValidationError("O resumo não deve ser maior que 500 caracteres.")
         return value
