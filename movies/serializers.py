df1544d3e16dc0ee42881b03ce030fe4433b087b
vitor <vitorvlofc@users.noreply.github.com>
2026-03-03 12:28:09 -0300

    165 - Endpoint com serializer

--- movies/serializers.py
+++ movies/serializers.py
@@ -27,3 +27,10 @@ class MovieModelSerializer(serializers.ModelSerializer):
         if len(value) > 500:
             raise serializers.ValidationError("O resumo não deve ser maior que 500 caracteres.")
         return value
+    
+
+class MovieStatsSerializer(serializers.Serializer):
+    total_movies = serializers.IntegerField()
+    movies_by_genre = serializers.ListField()
+    total_reviews = serializers.IntegerField()
+    average_stars = serializers.FloatField()
